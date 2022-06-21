9e79d5c4442b2ed7d1108fc0a953213458b64a32
joaowinderfeldbussolotto <joaowinderfeldbussolotto@gmail.com>
2022-06-20 22:04:54 -0300

    first commit

---
+++ arrayGenerator.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#define MAX 50000
+#define LEN 10
+
+int *generateRandomArray(int len, int mode)
+{
+  int *v = (int *)malloc(len * sizeof(int));
+  int i = 0;
+  srand(time(NULL));
+
+  for (; i < len; i++)
+  {
+    if (mode == 1)
+    {
+        *(v + i) = i;
+    }
+    else if (mode == 2)
+    {
+      *(v + i) = MAX -i;
+    }
+    else
+    {
+      *(v + i) = rand() % MAX;
+    }
+  }
+
+  return v;
+}
+
+int getLenghtFromTerminal(int argc, char *argv[])
+{
+  if (argc > 1)
+  {
+    char *charNum = argv[1];
+    return atoi(*(argv + 1));
+  }
+  else
+  {
+    return LEN;
+  }
+}
+
+void printArrayToFile(int *v, int len, FILE *file)
+{
+  int i = 0;
+  fprintf(file, "%d\n", len);
+
+  for (; i < len; i++)
+  {
+    fprintf(file, "%d\n", *(v + i));
+  }
+}
+char* createFileName(int mode, int lenght)
+{
+  char* filename = malloc(sizeof(char)*20);
+
+  if (mode == 1)
+  {
+    strcpy(filename,"crescente-");
+  }
+  else if (mode == 2)
+  {
+    strcpy(filename,"decrescente-");
+  }
+  else
+  {
+    strcpy(filename,"aleatorio-");
+  }
+  char strLenght[10];
+  sprintf(strLenght, "%d", lenght);
+  strcat(filename,strLenght);
+
+  return filename;
+}
+
+int main(int argc, char *argv[])
+{
+  int mode;
+  int lenght = getLenghtFromTerminal(argc, argv);
+  printf("Informe a ordem do vetor:\n 1 - crescente\n2 - decrescente\n3 - aleatorio\n");
+  scanf("%d", &mode);
+
+  int *v = generateRandomArray(lenght, mode);
+  FILE *f = fopen(createFileName(mode, lenght), "w");
+  printArrayToFile(v, lenght, f);
+  return 0;
+}
---
+++ trabalho1.c
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#define MAX 50000;
+
+int *generateRandomArray(int len, int mode)
+{
+  int *v = (int *)malloc(len * sizeof(int));
+  int i = 0;
+  srand(time(NULL));
+
+  for (; i < len; i++)
+  {
+    if (mode == 1)
+    {
+        *(v + i) = i;
+    }
+    else if (mode == 2)
+    {
+      *(v + i) = len-i;
+    }
+    else
+    {
+      *(v + i) = rand() % MAX;
+    }
+  }
+  return v;
+}
+
+
+void printArray(int *v, int len)
+{
+  int i = 0;
+  printf("[");
+  for (; i < len; i++)
+  {
+    printf("%d, ", *(v + i));
+  }
+  printf("]\n");
+}
+
+
+double getTimeInSeconds(struct timeval start, struct timeval stop){
+   return ((double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec));
+}
+
+
+
+
+void printInfo(char* methodName, struct timeval start, struct timeval stop, long long int numCompare, long long int numSwaps, int lenght){
+
+  printf("----%s----\n", methodName);
+  printf("Tamanho: %d\n", lenght);
+  //printf("Tempo: %lu (us) \n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+  //printf("Tempo: %f (s) \n", time);
+  printf("Tempo: %f (s) \n", getTimeInSeconds(start, stop));
+  printf("Numero de comparacoes: %lld\n", numCompare);
+  printf("Numero de trocas: %lld\n", numSwaps);
+}
+
+void swap(int i, int j, int *v)
+{
+  int aux = v[i];
+  v[i] = v[j];
+  v[j] = aux;
+}
+
+void bubbleSort(int *v, int len)
+{
+  struct timeval start, stop;
+  int i, j, aux;
+  long long int numCompare, numSwaps;
+  numCompare = numSwaps = 0;
+  int isInOrder = 1;
+
+  gettimeofday(&start, NULL);
+  for (i = len - 1; i > 0; i--)
+  {
+    for (j = 0; j < i; j++)
+    {
+      if (v[j] > v[j + 1])
+      {
+        isInOrder = 0;
+        swap(j, j + 1, v);
+        numSwaps++;
+      }
+      numCompare++;
+    }
+    if (isInOrder)
+    {
+      gettimeofday(&stop, NULL);
+      printInfo("Bubble sort", start, stop, numCompare, numSwaps, len);
+      return;
+    }
+  }
+  gettimeofday(&stop, NULL);
+  printInfo("Bubble sort", start, stop, numCompare, numSwaps, len);
+}
+
+void selectionSort(int *v, int len)
+{
+  struct timeval start, stop;
+  int i, j, min;
+  long long int numCompare, numSwaps;
+  numCompare = numSwaps = 0;
+  gettimeofday(&start, NULL);
+  for (j = 0; j < len - 1; j++)
+  {
+    min = j;
+    for (i = j + 1; i < len; i++)
+    {
+      if (v[i] < v[min])
+      {
+        min = i;
+      }
+      numCompare++;
+    }
+    if (min != j)
+    {
+      swap(j, min, v);
+      numSwaps++;
+    }
+  }
+  gettimeofday(&stop, NULL);
+
+  printInfo("Selection sort", start, stop, numCompare, numSwaps, len);
+}
+
+void insertionSort(int *v, int len)
+{
+   struct timeval start, stop;
+   int i,j;
+   long long int numCompare, numSwaps;
+   numCompare = numSwaps = 0;
+  gettimeofday(&start, NULL);
+   for (i = 1; i < len; i++){
+    j = i;
+    while (j > 0 && v[j-1] > v[j]){
+        numCompare++;
+        swap(j,j-1, v);
+        numSwaps++;
+        j--;
+    }
+    if(j!=0){
+      numCompare++;
+    }
+
+   }
+
+  gettimeofday(&stop, NULL);
+  printInfo("Insertion sort", start, stop, numCompare, numSwaps, len);
+
+}
+
+
+int main(int argc, char *argv[])
+{
+  int lenght;
+  scanf("%d", &lenght);
+  int *v =(int*) malloc(lenght * sizeof(int));
+  for(int i = 0; i < lenght; i++){
+    scanf("%d", v+i);
+  }
+  //bubbleSort(v, lenght);
+  //insertionSort(v, lenght);
+  selectionSort(v, lenght);
+  return 0;
+}
